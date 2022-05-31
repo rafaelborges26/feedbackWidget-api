073c5509bcdcee7e2ced6194a23398f9d21c08af
= <rafael.borges2698@gmail.com>
2022-05-31 14:29:43 -0300

    tryed again resolve error size body request

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,11 @@ import bodyParser from 'body-parser'
 
 const app = express()
 
-app.use(cors())
-app.use(express.json())
-
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+app.use(cors())
+app.use(express.json())
 
 app.use(routes)
 
